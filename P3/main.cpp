1600135552826cd9ba08b2bc8e81099a522f9c0b
alexmikkelsen <alexmikkelsen@gmail.com>
2015-10-15 08:33:44 +0200

    Second draft (YCrCb)

--- P3/main.cpp
+++ P3/main.cpp
@@ -9,8 +9,9 @@ using namespace cv;
 int main() {
 	Mat image, image2, image3, image4, image5;
 	Mat element = Mat::ones(10, 10, CV_8UC1);
-	//vector<Mat> channels;
-	int value;
+
+	vector<vector<Point> > contours;
+	vector<Vec4i> hierarchy;
 
 	VideoCapture cap(0);
 	namedWindow("Webcam");
@@ -19,25 +20,25 @@ int main() {
 	while (true){
 		cap >> image;
 
-		cvtColor(image, image2, CV_BGR2HSV);
-
-		//split(image2, channels);
-
-		// cv::threshold(channels[0],channels[0],40,80,CV_THRESH_BINARY);
-		//cv::threshold(channels[1],channels[1],0.23,0.68,CV_THRESH_BINARY);
+		cvtColor(image, image2, CV_BGR2YCrCb);
 
 		GaussianBlur(image2, image3, Size(5, 5), 0, 0);
-		inRange(image3, Scalar(0, 30, 60, 0), Scalar(20, 150, 255, 0), image4);
+		inRange(image3, Scalar(20, 140, 100, 0), Scalar(100, 200, 255, 0), image4);
 
 		erode(image4, image4, element);
 		dilate(image4, image4, element);
 
+		findContours(image4, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
 
-		// cv::threshold(image2,image2,120,255,CV_THRESH_BINARY);
+		/// Draw contours
+		Mat drawing = Mat::zeros(image.size(), CV_8UC3);
+		for (int i = 0; i< contours.size(); i++)
+		{
+			Scalar color(255, 255, 255);
+			drawContours(drawing, contours, i, color, 2, 8, hierarchy);
+		}
 
-		//merge(channels,image3);
-
-		imshow("Webcam", image4);
+		imshow("Webcam", drawing);
 		waitKey(1); // Wait for a key press to show another image
 	}
 }
