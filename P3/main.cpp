0cb9a673a44d26f1b4849bd8ffb6a671af88f3e3
alexmikkelsen <alexmikkelsen@gmail.com>
2015-10-16 09:20:09 +0200

    Third draft

--- P3/main.cpp
+++ P3/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 using namespace cv;
 
 int main() {
-	Mat image, image2, image3, image4, image5;
+	Mat image, image2, image3, image4, image5, image03;
 	Mat element = Mat::ones(10, 10, CV_8UC1);
 
 	vector<vector<Point> > contours;
@@ -20,22 +20,38 @@ int main() {
 	while (true){
 		cap >> image;
 
-		cvtColor(image, image2, CV_BGR2YCrCb);
+		cvtColor(image, image2, CV_BGR2HSV);
 
-		GaussianBlur(image2, image3, Size(5, 5), 0, 0);
-		inRange(image3, Scalar(20, 140, 100, 0), Scalar(100, 200, 255, 0), image4);
+		//GaussianBlur(image2, image3, Size(3, 3), 0, 0);
+
+
+		inRange(image2, Scalar(20, 100, 50, 0), Scalar(100, 200, 150, 0), image3);
+
+		medianBlur(image3, image4, 3);
 
 		erode(image4, image4, element);
 		dilate(image4, image4, element);
 
+		//imshow("Webfdcam", image4);
+
 		findContours(image4, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+
+		/// Find the convex hull object for each contour
+		vector<vector<Point> >hull(contours.size());
+		//vector<Vec4i> convexityDefects;
+		for (int i = 0; i < contours.size(); i++)
+		{
+			convexHull(Mat(contours[i]), hull[i], false);
+			//convexityDefects(Mat(contours[i]), hull[i], convexityDefects);
+		}
 
 		/// Draw contours
 		Mat drawing = Mat::zeros(image.size(), CV_8UC3);
 		for (int i = 0; i< contours.size(); i++)
 		{
 			Scalar color(255, 255, 255);
-			drawContours(drawing, contours, i, color, 2, 8, hierarchy);
+			drawContours(drawing, contours, i, color, CV_FILLED, 8, hierarchy, 0, Point());
+			drawContours(drawing, hull, i, color, 3, 8, vector<Vec4i>(), 0, Point());
 		}
 
 		imshow("Webcam", drawing);
