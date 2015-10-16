0a1c66904884001e2059c6fc94eff98b25484b47
alexmikkelsen <alexmikkelsen@gmail.com>
2015-10-16 12:44:34 +0200

    Added trackbars

--- P3/main.cpp
+++ P3/main.cpp
@@ -6,6 +6,36 @@
 using namespace std;
 using namespace cv;
 
+int H_MIN = 0; //Ideal value = 35
+int H_MAX = 255; //Ideal value = 82
+int S_MIN = 0; //Ideal value = 40
+int S_MAX = 255;
+int V_MIN = 0;
+int V_MAX = 255;
+
+void on_trackbar(int, void*){
+
+}
+
+void trackbars(){
+	namedWindow("Trackbars", 0);
+
+	char TrackbarName[50];
+	sprintf(TrackbarName, "H_MIN", H_MIN);
+	sprintf(TrackbarName, "H_MAX", H_MAX);
+	sprintf(TrackbarName, "S_MIN", S_MIN);
+	sprintf(TrackbarName, "S_MAX", S_MAX);
+	sprintf(TrackbarName, "V_MIN", V_MIN);
+	sprintf(TrackbarName, "V_MAX", V_MAX);
+
+	createTrackbar("H_MIN", "Trackbars", &H_MIN, H_MAX, on_trackbar);
+	createTrackbar("H_MAX", "Trackbars", &H_MAX, H_MAX, on_trackbar);
+	createTrackbar("S_MIN", "Trackbars", &S_MIN, S_MAX, on_trackbar);
+	createTrackbar("S_MAX", "Trackbars", &S_MAX, S_MAX, on_trackbar);
+	createTrackbar("V_MIN", "Trackbars", &V_MIN, V_MAX, on_trackbar);
+	createTrackbar("V_MAX", "Trackbars", &V_MAX, V_MAX, on_trackbar);
+}
+
 int main() {
 	Mat image, image2, image3, image4, image5, image03;
 	Mat element = Mat::ones(10, 10, CV_8UC1);
@@ -16,16 +46,20 @@ int main() {
 	VideoCapture cap(0);
 	namedWindow("Webcam");
 
+	trackbars();
+
 	waitKey(200);
 	while (true){
 		cap >> image;
+
+
 
 		cvtColor(image, image2, CV_BGR2HSV);
 
 		//GaussianBlur(image2, image3, Size(3, 3), 0, 0);
 
 
-		inRange(image2, Scalar(20, 100, 50, 0), Scalar(100, 200, 150, 0), image3);
+		inRange(image2, Scalar(H_MIN, S_MIN, V_MIN, 0), Scalar(H_MAX, S_MAX, V_MAX, 0), image3);
 
 		medianBlur(image3, image4, 3);
 
@@ -38,11 +72,11 @@ int main() {
 
 		/// Find the convex hull object for each contour
 		vector<vector<Point> >hull(contours.size());
-		//vector<Vec4i> convexityDefects;
+		//vector<Vec4i> convexityDefects1;
 		for (int i = 0; i < contours.size(); i++)
 		{
 			convexHull(Mat(contours[i]), hull[i], false);
-			//convexityDefects(Mat(contours[i]), hull[i], convexityDefects);
+			//convexityDefects(Mat(contours[i]), hull[i], convexityDefects1);
 		}
 
 		/// Draw contours
